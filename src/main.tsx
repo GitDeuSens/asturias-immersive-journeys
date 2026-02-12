a9fa38dcca3bd0f68b48016c17486931613271f1
Timofei Starovoitov <tstarovoitov@deusens.com>
2026-02-12 12:53:20 +0100

    fix

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
 import { I18nextProvider } from 'react-i18next';
 import { QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
@@ -17,13 +18,15 @@ loadDirectusTranslations().catch(err => {
 });
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
-  <I18nextProvider i18n={i18n}>
-    <QueryClientProvider client={queryClient}>
-      <ReactQueryDevtools initialIsOpen={false} />
-      <ErrorBoundary>
-        <Toaster />
-        <App />
-      </ErrorBoundary>
-    </QueryClientProvider>
-  </I18nextProvider>
+  <BrowserRouter>
+    <I18nextProvider i18n={i18n}>
+      <QueryClientProvider client={queryClient}>
+        <ReactQueryDevtools initialIsOpen={false} />
+        <ErrorBoundary>
+          <Toaster />
+          <App />
+        </ErrorBoundary>
+      </QueryClientProvider>
+    </I18nextProvider>
+  </BrowserRouter>
 );
